23983ef6e893c484b76f2b962a34a84ff7572f3b
BraylinP <brperazaal@est.utn.ac.cr>
2025-12-12 09:31:26 -0600

    Corrección tramites

--- models/tramite.model.js
+++ models/tramite.model.js
@@ -1,4 +1,7 @@
 const db = require('../db.js');
+
+const defaultFields = 'id, title, description, status, user_id, create_date, last_update_date';
+
 async function createTramite({ title, description, user_id }) {
 const res = await db.query(
 `INSERT INTO tramites (title, description, user_id) VALUES ($1, $2, $3) RETURNING ${defaultFields}`,
